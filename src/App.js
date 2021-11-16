d8aa7cf8518e1bf6712ee6d3a04a766010ecc60e
Amalia <amaliaroye@gmail.com>
2021-11-16 15:39:20 -0500

    Add canvas and palette

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import Canvas from './Canvas'
+import Palette from './Palette'
 
-function App() {
+export default function App() {
+  const [foreground, setForeground] = useState('#000000')
+  const [background, setBackground] = useState('#ffffff')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Canvas currentColor={foreground} />
+      <Palette
+        foreground={foreground}
+        background={background}
+        setForeground={setForeground}
+        setBackground={setBackground}
+      />
     </div>
-  );
+  )
 }
-
-export default App;
