748c7d85b3e00dbcd18ee6af5e14119fe039cbfb
Amalia <amaliaroye@gmail.com>
2021-11-24 10:44:25 -0500

    Format code

--- src/Palette.js
+++ src/Palette.js
@@ -31,47 +31,7 @@ const COLORS = [
   '#FC047C',
   '#FE7C36',
 ]
-const SWATCH_SIZE = 24
-
-const Swatch = styled.button`
-  width: ${SWATCH_SIZE}px;
-  height: ${SWATCH_SIZE}px;
-  background-color: ${(props) => props.color};
-  cursor: pointer;
-  border-style: inset;
-`
-
-const CurrentSwatch = styled(Swatch)`
-  grid-column: ${(props) => (props.foreground ? '1 / span 2' : '2 / span 2')};
-  grid-row: ${(props) => (props.foreground ? '1 / span 2' : '2 / span 2')};
-  z-index: ${(props) => (props.active ? 5 : 4)};
-`
-
-const CurrentWrapper = styled.div`
-  display: grid;
-  grid-template-columns: repeat(3, calc((${SWATCH_SIZE} / 2) * 1px));
-  grid-template-rows: repeat(3, calc((${SWATCH_SIZE} / 2) * 1px));
-  background-color: var(--light-gray);
-  padding: 6px;
-  border-style: inset;
-`
-
-const SwatchWrapper = styled.div`
-  display: grid;
-  grid-template-rows: repeat(2, 1fr);
-  grid-template-columns: repeat(14, 1fr);
-  gap: 3px;
-`
-
-const PaletteWrapper = styled.div`
-  position: sticky;
-  bottom: 0;
-  display: flex;
-  align-items: center;
-  background-color: var(--gray);
-  padding: 4px;
-  gap: 12px;
-`
+const SWATCH_SIZE = 20
 
 export default function Palette({
   foreground,
@@ -116,3 +76,43 @@ export default function Palette({
     </PaletteWrapper>
   )
 }
+
+const Swatch = styled.button`
+  width: ${SWATCH_SIZE}px;
+  height: ${SWATCH_SIZE}px;
+  background-color: ${(props) => props.color};
+  cursor: pointer;
+  border-style: inset;
+`
+
+const CurrentSwatch = styled(Swatch)`
+  grid-column: ${(props) => (props.foreground ? '1 / span 2' : '2 / span 2')};
+  grid-row: ${(props) => (props.foreground ? '1 / span 2' : '2 / span 2')};
+  z-index: ${(props) => (props.active ? 5 : 4)};
+`
+
+const CurrentWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, calc((${SWATCH_SIZE} / 2) * 1px));
+  grid-template-rows: repeat(3, calc((${SWATCH_SIZE} / 2) * 1px));
+  background-color: var(--light-gray);
+  padding: 6px;
+  border-style: inset;
+`
+
+const SwatchWrapper = styled.div`
+  display: grid;
+  grid-template-rows: repeat(2, 1fr);
+  grid-template-columns: repeat(14, 1fr);
+  gap: 3px;
+`
+
+const PaletteWrapper = styled.div`
+  position: sticky;
+  bottom: 0;
+  display: flex;
+  align-items: center;
+  background-color: var(--gray);
+  padding: 4px;
+  gap: 12px;
+`
